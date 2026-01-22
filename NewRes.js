685c439dae48906efaaaf37f737c915966f8e5c9
B!guU <biguueditz@gmail.com>
2026-01-22 17:28:23 +0530

    Create NewRes.js

---
+++ NewRes.js
@@ -0,0 +1,46 @@
+{
+  "user": {
+    "id": 2070636213,
+    "first_name": "",
+    "last_name": "",
+    "username": "426a3e52",
+    "city": "",
+    "creatorPro": [],
+    "state": "",
+    "country": "",
+    "location": "",
+    "company": "",
+    "occupation": "",
+    "created_on": 1761125298,
+    "url": "#",
+    "images": {
+      "50": "https://pps.services.adobe.com/api/profile/image/default/ecff3be2-c283-45ef-9985-acc15eec612e/50",
+      "100": "https://pps.services.adobe.com/api/profile/image/default/ecff3be2-c283-45ef-9985-acc15eec612e/100",
+      "115": "https://pps.services.adobe.com/api/profile/image/default/ecff3be2-c283-45ef-9985-acc15eec612e/115",
+      "138": "https://pps.services.adobe.com/api/profile/image/default/ecff3be2-c283-45ef-9985-acc15eec612e/138",
+      "230": "https://pps.services.adobe.com/api/profile/image/default/ecff3be2-c283-45ef-9985-acc15eec612e/230",
+      "276": "https://pps.services.adobe.com/api/profile/image/default/ecff3be2-c283-45ef-9985-acc15eec612e/276"
+    },
+    "display_name": "",
+    "fields": [],
+    "has_default_image": 1,
+    "website": "#",
+    "banner_image_url": "",
+    "has_premium_access": true,
+    "stats": {
+      "followers": 0,
+      "following": 0,
+      "comments": 0,
+      "appreciations": 0,
+      "views": 0,
+      "team_members": true
+    },
+    "adobe_id": "ECBE22C468F8A3AA0A495EA3@AdobeID",
+    "twitter": "",
+    "links": [],
+    "sections": {},
+    "social_links": [],
+    "has_social_links": false
+  },
+  "http_code": 200
+}
